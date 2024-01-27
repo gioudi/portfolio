47a75cf74ba3b933d106f3c30d44ecabfe1896ce
gioudi <sergiopenagos881@gmail.com>
2024-01-27 18:13:34 -0500

    feat/time-line-component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+// src/main.ts
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import { createI18n } from "vue-i18n";
+import ScrollReveal from "vue-scroll-reveal";
+import VerticalTimeline from "vue-vertical-timeline";
+import VerticalTimelineItem from "vue-vertical-timeline-item";
+import "./styles/global.scss";
 
 const app = createApp(App);
 
@@ -10,17 +16,23 @@ app.use(createPinia());
 app.use(router);
 
 const i18n = createI18n({
-  locale: "en", // Default locale
+  locale: "en",
   messages: {
-    en: {
-      // Add your English translations
-    },
-    es: {
-      // Add your Spanish translations
-    },
+    en: {},
+    es: {},
   },
 });
 
 app.use(i18n);
 
+app.use(ScrollReveal());
+app.component("VerticalTimeline", VerticalTimeline);
+app.component("VerticalTimelineItem", VerticalTimelineItem);
+
+// Configure scroll animation for the timeline
+app.directive("scroll-reveal", {
+  beforeMount(el, binding) {
+    ScrollReveal().reveal(el, binding.value);
+  },
+});
 app.mount("#app");
