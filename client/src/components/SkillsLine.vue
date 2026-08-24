aef2dee96218344c0c9ec80b6b1021df728661ae
gioudi <sergiopenagos881@gmail.com>
2026-08-24 14:58:30 -0500

    fix(JOR-009): lock skills to a single four-column row with fixed quarters

--- client/src/components/SkillsLine.vue
+++ client/src/components/SkillsLine.vue
@@ -3,11 +3,11 @@
     <h2 id="skills" class="title is-2 has-text-centered">
       {{ t("skills.heading") }}
     </h2>
-    <div class="columns is-multiline is-mobile">
+    <div class="columns is-multiline is-variable is-2">
       <div
         v-for="group in skillGroups"
         :key="group.titleKey"
-        class="skills-section column is-half-mobile is-3-tablet is-3-desktop"
+        class="skills-section column is-half-mobile is-3"
         data-aos="fade-up"
       >
         <h3 class="subtitle is-4">{{ t(group.titleKey) }}</h3>
@@ -85,6 +85,10 @@ const skillGroups = [
 <style lang="scss">
 .skill-row {
   margin-bottom: 0.6rem;
+}
+
+.skills-section {
+  min-width: 0;
 }
 
 .container-icon {
